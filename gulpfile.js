137031b68da2efee320fb50f96af077add183dcc
Azai <azai@SunnyMacBookAir.local>
2016-12-22 16:38:07 +0800

    Minimal gulp task; 1st page with mock data

--- gulpfile.js
+++ gulpfile.js
@@ -9,49 +9,37 @@ var inlinesource = require('gulp-inline-source');
 var jshint = require('gulp-jshint');
 var jsonlint = require("gulp-jsonlint");
 var less = require('gulp-less');
-var minifyCSS = require('gulp-minify-css');
+var minifyCSS = require('gulp-clean-css');
 var minifyHTML = require('gulp-minify-html');
 var path = require('path');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
-var cssfiles = ['./src/css/normalize.css', './src/css/bricks.css'];
 var jsfiles = ['./src/js/script.js'];
-
-gulp.task('less', function() {
-    return gulp.src('./src/less/**/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./src/css/'));
-});
+var base64Opts = {
+    extensions: ['png', /\.jpg#datauri$/i]
+};
 
 // Concat and compress CSS files in src/data/css, and generate build/production.css
-gulp.task('minify-css', ['less'], function() {
+gulp.task('css', function() {
     var opts = {
         keepBreaks: false,
         compatibility: 'ie8',
         keepSpecialComments: 0
     };
-    var cssfilesToBuild = cssfiles;
-    return gulp.src(cssfilesToBuild)
+
+    return gulp.src('./src/less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
     .pipe(concat('production.css'))
-    .pipe(base64())
+    .pipe(base64(base64Opts))
     .pipe(minifyCSS(opts))
     .pipe(gulp.dest('./src/rendered'));
 });
 
-// Copy build/production.js and build/production.css into src/html/index.html, compress html, and generate build/index.html
-gulp.task('copy-css', ['less'], function () {
-    var cssfilesToBuild = cssfiles;
-    return gulp.src(cssfilesToBuild)
-    .pipe(concat('production.css'))
-    .pipe(base64())
-    .pipe(gulp.dest('./src/rendered'));
-});
-
 // Concat and compress JS files in src/data/javascript, and generate build/production.js
-gulp.task('minify-js', function () {
+gulp.task('js', function () {
     var jsfilesToBuild;
     if (jsfiles === [] || typeof jsfiles === 'undefined') {
         jsfilesToBuild = './src/js/production.js';
@@ -64,12 +52,6 @@ gulp.task('minify-js', function () {
     .pipe(gulp.dest('./src/rendered'));
 });
 
-// Copy build/production.js and build/production.css into src/html/index.html, compress html, and generate build/index.html
-gulp.task('copy-js', function () {
-    return gulp.src('./src/js/*.js')
-    .pipe(gulp.dest('./src/rendered'));
-});
-
 // Validate src/data JSON files
 gulp.task('jsonlint', function () {
     return gulp.src('./src/data/*.json')
@@ -78,7 +60,7 @@ gulp.task('jsonlint', function () {
 });
 
 // Render html using data in src/data against src/templates, and build src/html/index.html
-gulp.task('render-template', ['jsonlint'], function (done) {
+gulp.task('template', ['jsonlint'], function (done) {
    var filepath = path.join(__dirname, './src/data/data-structure.json');
    var options = {
        ignorePartials: true,
@@ -101,7 +83,7 @@ gulp.task('render-template', ['jsonlint'], function (done) {
    });
 
 });
-gulp.task('build', ['render-template', 'minify-js', 'minify-css'], function () {
+gulp.task('build', ['template', 'css'], function () {
     var optsHtml = {
       conditionals: true,
       spare: true
@@ -111,15 +93,9 @@ gulp.task('build', ['render-template', 'minify-js', 'minify-css'], function () {
     };
 
     return gulp.src('./src/rendered/*.html')
-    .pipe(base64())
+    .pipe(base64(base64Opts))
     .pipe(inlinesource(optsInline))
     .pipe(minifyHTML(optsHtml))
-    .pipe(gulp.dest('.'));
-});
-
-// Copy build/production.js and build/production.css into src/html/index.html, compress html, and generate build/index.html
-gulp.task('copy-and-compress', ['render-template', 'minify-js', 'minify-css'], function (done) {
-    return gulp.src('./src/rendered/*')
     .pipe(gulp.dest('.'));
 });
 
